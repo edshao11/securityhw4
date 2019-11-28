458de44dff2e87cbcfe192387e51b94194a500f5
Ed Shao <ss5902@columbia.edu>
2019-11-27 22:19:11 -0700

    a

--- part0/part0.cpp
+++ part0/part0.cpp
@@ -1,16 +1,18 @@
 #include <iostream>
+
 using namespace std;
+
 long factorial(int n);
+
 int main()
 {
     int n(0);
-    cout << ”Please enter a positive integer : ” << endl;
-    cin >> n;
-    long val = factorial(n);
-    cout << ”The factorial is : ” << val << endl;
-    cin.get();
-    return 0;
+    cout << "please enter a positive integer: " << endl;
+    cin >> n long val = factorial(n);
+    cout << "The factorial is: " << val << endl;
+    cin.get() return 0;
 }
+
 long factorial(int n)
 {
     long result(1);
